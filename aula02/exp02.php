80c057644dacca00eb41709bf6753a5ad6775094
felipe-castro <146869934+felipe-castro157@users.noreply.github.com>
2023-10-18 17:20:17 -0300

    aula2

---
+++ aula02/exp02.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    
+     <h1>titulo</h1>
+    <p class="estilo"> classe 01</p>
+
+    <div class="conteudo">
+
+    <p>
+     
+    <p> titulo da div</p>
+
+    <p id="titulo"> texto dentro de subconteudo</p>
+
+    
+    </div>
+
+
+
+
+
+</body>
+</html>
