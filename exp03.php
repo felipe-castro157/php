e7c45e2fbdc67d936bb0c46d67256b9c41ec7786
felipe-castro <146869934+felipe-castro157@users.noreply.github.com>
2023-11-08 13:38:45 -0300

    Add files via upload

---
+++ exp03.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exp03</title>
+    <link rel="stylesheet" href="exp03.css">
+</head>
+<body>
+    <div id="o">
+     </div>
+    <div id="w"> 
+        
+    </div>
+    <div id="s"> 
+
+    </div>
+    <div id="a">
+        
+</div>
+
+</body>
+</html>
