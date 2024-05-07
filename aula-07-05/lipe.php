afcda517f158892632cfeff0d946667ae8664440
felipe-castro <146869934+felipe-castro157@users.noreply.github.com>
2024-05-07 09:54:13 -0300

    Add files via upload

---
+++ aula-07-05/lipe.php
@@ -0,0 +1,45 @@
+<?php
+
+$aluno["nome"] = "felipe castro";
+$aluno["endereco"] = "Rua C";
+$aluno["cidade"] = "guanambi";
+$aluno["estado"] = "bahia";
+$aluno["idade"] = "22";
+$aluno["cpf"] = "111.278.098-78";
+
+$aluno1["nome"] = "Tunegas";
+$aluno1["endereco"] = "Avenida Sao paulo";
+$aluno1["cidade"] = "salvador";
+$aluno1["estado"] = "bahia";
+$aluno1["idade"] = "19";
+$aluno1["cpf"] = "081.525.447-90";
+
+$aluno2["nome"] = "Lucas";
+$aluno2["endereco"] = "Rua J";
+$aluno2["cidade"] = "Vitoria da Conquista";
+$aluno2["estado"] = "Bahia";
+$aluno2["idade"] = "80";
+$aluno2["cpf"] = "978.978.909-9";
+
+$professor["nome"] = "Pimentel";
+$professor["endereco"] = "Rua Casimiro de Abreu";
+$professor["cidade"] = "Monte Azul";
+$professor["estado"] = "Minas Gerais";
+$professor["idade"] = "25";
+$professor["materia"] = "banco de dados";
+
+$professor1["nome"] = "fabio";
+$professor1["endereco"] = "Rua 29";
+$professor1["cidade"] = "guanambi";
+$professor1["estado"] = "bahia";
+$professor1["idade"] = "70";
+$professor1["materia"] = "progamador";
+
+$professor1["nome"] = "silvia";
+$professor1["endereco"] = "casa dela";
+$professor1["cidade"] = "rocha";
+$professor1["estado"] = "pangeia";
+$professor1["idade"] = "99";
+$professor1["materia"] = "portugues";
+
+?>
